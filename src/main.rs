c79a4a2141337744082beab47aa14a7e4bb66a11
Niki4tap <rombiklol2@gmail.com>
2023-02-04 04:20:08 +0300

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,11 @@ fn main() {
 		"enc" => {
 			let dna = OwnedDNA::try_from_nucleotide_str(&args[1]).unwrap();
 			println!("{}", dna.try_to_byte_str().unwrap());
-		},
+		}
 		"dec" => {
 			let dna = OwnedDNA::try_from_byte_str(&args[1]).unwrap();
 			println!("{}", dna.to_nucleotide_str());
-		},
+		}
 		_ => {
 			panic!("unrecognized command");
 		}
